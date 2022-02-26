e4bdbbf966b33d9c5089965c18600757c78a689c
Rawane <rawane.meissa@gmail.com>
2022-02-26 14:22:47 +0000

    création du front controller

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+    //inclusion des chemins absolus
+    require_once dirname(dirname(__FILE__)).'/config/constantes.php';
+    //inclusion du Validator
+    require_once dirname(dirname(__FILE__))."/config/validator.php";
+    //inclusion du convertisseur
+    require_once dirname(dirname(__FILE__))."/config/orm.php";
+    //inclusion des roles
+    require_once dirname(dirname(__FILE__))."/config/role.php";
+    //Chargement du router
+    require_once dirname(dirname(__FILE__))."/config/router.php";
+
---
+++ src/controllers/securite.controllers.php
@@ -0,0 +1,15 @@
+<?php
+/**
+* Traitement des Requetes POST
+*/
+if($_SERVER['REQUEST_METHOD']=="POST"){
+if(isset($_POST['action'])){
+}
+}
+/**
+* Traitement des Requetes GET
+*/
+if($_SERVER['REQUEST_METHOD']=="GET"){
+if(isset($_GET['action'])){
+}
+}
