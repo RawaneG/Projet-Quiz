fd9bf023bb7c97ca8e75dffb2cf089f64986c0f2
Rawane <rawane.meissa@gmail.com>
2022-02-26 16:30:32 +0000

    création du router

--- src/controllers/securite.controllers.php
+++ src/controllers/securite.controllers.php
@@ -2,14 +2,20 @@
 /**
 * Traitement des Requetes POST
 */
-if($_SERVER['REQUEST_METHOD']=="POST"){
-if(isset($_POST['action'])){
-}
+if($_SERVER['REQUEST_METHOD'] == "POST")
+{
+    if(isset($_POST['action']))
+    {
+
+    }
 }
 /**
 * Traitement des Requetes GET
 */
-if($_SERVER['REQUEST_METHOD']=="GET"){
-if(isset($_GET['action'])){
-}
+if($_SERVER['REQUEST_METHOD'] == "GET")
+{
+    if(isset($_GET['action']))
+    {
+
+    }
 }
